ecc6eef8bb1b1d9821647a34a7a83c79ea67b1c1
Abdur Rehman <abdur4082@gmail.com>
2020-07-18 10:03:43 +0500

    Fix ngx-translate/core version issue

---
+++ src/app/team-management/team-management.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddTeamComponent } from './add-team/add-team.component';
+
+
+
+@NgModule({
+  declarations: [AddTeamComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TeamManagementModule { }
